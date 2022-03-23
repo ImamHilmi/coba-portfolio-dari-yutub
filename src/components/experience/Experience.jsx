12f0a6d6565857a8883ad73b776dc33abf48c5ff
Imam Hilmi <imam.hilmi253@gmail.com>
2022-03-23 15:04:54 +0700

    Update Experience Content Section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,119 @@
 import React from 'react'
 import "./Experience.css"
+import { CgCheckR } from "react-icons/cg"
 
 const Experience = () => {
   return (
     <section id="experience">
-      experience
+      <h5 className="title">The Skills That I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+
+      {/* FrontEnd Section */}
+
+        <div className="experience__frontend">
+          <h3>FrontEnd Development</h3>
+            <div className="experience__content">
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>CSS</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>JavaScript</h4>
+                  <small className="text-light">Basic</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>Wordpress</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>React</h4>
+                  <small className="text-light">Basic</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>Bootstrap</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+
+            </div>
+        </div>
+
+      {/* End of FrontEnd Section */}
+
+      {/* BackEnd Section */}
+
+        <div className="experience__backend">
+        <h3>BackEnd Development</h3>
+            <div className="experience__content">
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>MySql</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>Java</h4>
+                  <small className="text-light">Basic</small>
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>Golang</h4>
+                  <small className="text-light">Basic</small> 
+                </div>
+              </article>
+
+              <article className="experience__details">
+                <CgCheckR className="experience__details-icon" />
+                <div>
+                  <h4>Postgres</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+
+            </div>
+        </div>
+
+      {/* End of BackEnd Section */}
+
+      </div>
+
     </section>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,6 +13,7 @@ const Header = () => {
         <h5 className="text-light">
           FrontEnd Developer
         </h5>
+        
         <CTA />
         <HeaderSocials />
 
