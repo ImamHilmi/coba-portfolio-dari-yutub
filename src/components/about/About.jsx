707f208568a8bc347414ef84c33385dd1814d4a5
Imam Hilmi <imam.hilmi253@gmail.com>
2022-03-23 14:06:32 +0700

    Update About Content Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import "./About.css"
+import ME from '../../assets/me1-1.jpg'
+import { BsAwardFill } from 'react-icons/bs'
+import { FaUsers } from 'react-icons/fa'
+import { MdOutlineWeb } from 'react-icons/md'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="My Pict" />
+          </div>
+        </div>
+
+        <div className="about__content">
+
+          <div className="about__cards">
+            <article className="about-card">
+              <BsAwardFill className="about__icon"/>
+              <h5>Experiences</h5>
+              <small>1 Years Working</small>
+            </article>
+
+            <article className="about-card">
+              <FaUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>2+ IT Company</small>
+            </article>
+
+            <article className="about-card">
+              <MdOutlineWeb className="about__icon"/>
+              <h5>Projects</h5>
+              <small>10+ Website Developed</small>
+            </article>
+          </div>
+
+          <p>
+            I'm quite proficient in UI/UX Design Application and Website. Also I have participated in several competitions related to information technology innovation. I have an interest to continue developing my designing ablities for any applications.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Nav.css"
+import { AiOutlineHome } from "react-icons/ai"
+import { FaUserGraduate } from "react-icons/fa"
+import { ImBook } from "react-icons/im"
+import { RiServiceFill, RiContactsFill } from "react-icons/ri"
 
 const Nav = () => {
+
+  const[activeNav, setActiveNav] = useState('#home');
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}>
+        <AiOutlineHome />
+      </a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>
+        <FaUserGraduate />
+      </a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>
+        <ImBook />
+      </a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>
+        <RiServiceFill />
+      </a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
+        <RiContactsFill />
+      </a>
+    </nav>
   )
 }
 
